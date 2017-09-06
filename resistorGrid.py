619dc50260a9c562cab3e9b8e69a10d96b02acce
Leo Browning <leobrowning92@gmail.com>
2017-09-06 13:24:29 +1200

    made result show correct 0V at gnd
    also changed syntax to make symbols in functions more explicit

--- resistorGrid.py
+++ resistorGrid.py
@@ -3,21 +3,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 class Network(object):
-    def __init__(self,network_rows, network_columns, ground_nodes=[-1], voltage_source_nodes=np.array([[0,5]])):
+    def __init__(self,network_rows, network_columns, ground_nodes=[-1], voltage_sources=np.array([[0,5]])):
         self.network_rows=network_rows
         self.network_columns=network_columns
         self.network_size=self.network_rows*self.network_columns
-        self.ground_nodes=ground_nodes
-        self.voltage_source_nodes=voltage_source_nodes
-        self.grid_connections=self.make_grid_connections(self.getG, self.network_rows, self.network_columns)
-        # to make the matrices necessary for the MNA matrix equation
-        self.mna_A=self.make_A(self.grid_connections, self.ground_nodes, self.voltage_source_nodes)
-        self.mna_z=self.make_z(self.network_rows, self.network_columns, self.voltage_source_nodes)
+        if all(x>=0 for x in ground_nodes):
+            self.ground_nodes=ground_nodes
+        else:
+            self.ground_nodes=[self.network_size-1]
 
+        self.voltage_sources=voltage_sources
+        self.grid_connections=self.make_grid_connections(self.getG)
+        # to make the matrices necessary for the MNA matrix equation
+        self.mna_A=self.make_A()
+        self.mna_z=self.make_z()
+        self.mna_x=np.matmul(np.linalg.inv(self.mna_A),self.mna_z)
+        self.node_voltages=self.get_voltages()
     def getG(self, n1,n2):
         """holds enough information to reconstruct r1,c1 to r2,c2 information which equates to physical position"""
         return 1
-    def make_grid_connections(self, getG,r,c):
+    def make_grid_connections(self, getG):
         """makes the array that encodes the connections between nodes and
         their conductance for a grid where every node is connected to its
         neerest neighbor.
@@ -30,6 +35,9 @@ class Network(object):
         j=i-1 is the conductance to the node to the left of point i
         j=i-c is the conductance to the node above point i
         j=i+c is the conductance to the node below point i"""
+        # define symbols
+        r=self.network_rows
+        c=self.network_columns
         n=r*c
         Gvalues=np.zeros((n,n))
         for i in range(n):
@@ -58,17 +66,17 @@ class Network(object):
                     if j in [i+1,i-1,i+c,i-c]:
                         Gvalues[i,j]=getG(i,j)
         return Gvalues
-
-
-    def make_A(self, connections,gnd,Vsrc):
-        G=connections
-        n=len(G)
+    def make_A(self):
+        # define symbols
+        gnd=self.ground_nodes
+        Vsrc=self.voltage_sources
+        G=self.grid_connections
+        n=self.network_size
         for i in range(n):
             for j in range(n):
                 if i==j:
-                    G[i,j]=-sum(connections[i])
-
-        G=np.delete(np.delete(G,gnd[0],0),gnd[0],1)
+                    G[i,j]=-sum(G[i])
+        G=np.delete(np.delete(G,gnd,0),gnd,1)
         n=len(G)
         B=np.zeros((n,len(Vsrc)))
         for i in range(n):
@@ -77,28 +85,25 @@ class Network(object):
         D=np.zeros((len(Vsrc),len(Vsrc)))
         BTD=np.append(B.T,D,axis=1)
         return np.append(np.append(G,B,axis=1),BTD,axis=0)
-
-    def make_z(self, r,c,Vsrc):
+    def make_z(self):
+        r=self.network_rows
+        c=self.network_columns
+        Vsrc=self.voltage_sources
         return np.append(np.zeros((r*c-1,1)),Vsrc[:,1][:,None],axis=0)
-    def solve_mna(self,show=True):
-        x=np.matmul(np.linalg.inv(self.mna_A),self.mna_z)
-        if show:
-            sns.heatmap(np.reshape(x,(self.network_rows, self.network_columns)), linewidths=1, linecolor='grey', annot=True)
-            plt.show()
+    def get_voltages(self):
+        # inserts the ground voltages back into x
+        x=np.insert(self.mna_x,self.ground_nodes,0,axis=0)
+        #splits the source currents from x
+        x=x[:-len(self.voltage_sources)]
+
+
         return x
 
 
 
-# r=2
-# c=3
-# n=r*c
-# gnd=[n-1]
-# Vsrc=np.array([[0,5]])
-# z=make_z(r,c,Vsrc)
-#
-# A=make_A(make_G(make_gridpattern(getG,r,c),gnd),Vsrc[:,0])
-# x=np.matmul(np.linalg.inv(A),z)
-# sns.heatmap(np.reshape(x,(r,c)),linewidths=1,linecolor='grey',annot=True)
-# plt.show()
-net=Network(2,3)
-print(net.solve_mna())
+
+
+net=Network(5,5)
+
+sns.heatmap(np.reshape(net.node_voltages,(net.network_rows, net.network_columns)), linewidths=1, linecolor='grey', annot=True)
+plt.show()
