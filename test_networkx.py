d2c4c215ddfdb80153b55a90f4ba2b6a19435a39
Leo Browning <leobrowning92@gmail.com>
2017-09-12 15:28:50 +1200

    added a bit mor functionality to nx test

--- test_networkx.py
+++ test_networkx.py
@@ -1,10 +1,20 @@
-
-
 import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
 G=nx.grid_2d_graph(3,3)
 pos={(n//3,n%3):np.array([n//3,n%3]) for n in range(9)}
+for edge in G.edges():
+    G.edge[edge[0]][edge[1]]['conductance']=np.random.uniform()
+    d=[]
+for n1,n2,data in G.edges(data=True):
+    d.append(data['conductance'])
+
 
 # This returns the correct adjacency matrix for the network
-nx.to_numpy_matrix(G,nodelist=sorted(G.nodes()))
-nx.draw(G,pos)
-plt.show
+(nx.to_numpy_matrix(G,nodelist=sorted(G.nodes()),weight='conductance')
+nx.draw_networkx(G,pos,width=2,edge_cmap=plt.get_cmap('plasma'),edge_vmin=0,edge_vmax=1,edge_color=d)
+
+plt.show()
