ecc4efdb18f604a551f99f70681fe801f7362613
Leo Browning <leobrowning92@gmail.com>
2018-07-17 16:31:39 +1200

    changing filename

--- slurm/test500_batchmeasure.sh
+++ slurm/test500_batchmeasure.sh
@@ -4,8 +4,8 @@ mkdir linexp
 cd linexp
 for x in {1..10}
 do
-    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s  -n 500 --scaling 5 --onoffmap 0 --element 1 --seed $x' > measurenet.sh
-    subpy -P 1 -t 24:00:00 measurenet.sh
+    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s  -n 500 --scaling 5 --onoffmap 0 --element 1 --seed '$x > mnet$x.sh
+    subpy -P 1 -t 24:00:00 mnet$x.sh
 done
 cd ..
 
@@ -13,7 +13,7 @@ mkdir fd
 cd fd
 for x in {1..10}
 do
-    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s  -n 500 --scaling 5 --onoffmap 0 --element 0 --seed $x' > measurenet.sh
-    subpy -P 1 -t 24:00:00 measurenet.sh
+    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s  -n 500 --scaling 5 --onoffmap 0 --element 0 --seed '$x > mnet$x.sh
+    subpy -P 1 -t 24:00:00 mnet$x.sh
 done
 cd ..
