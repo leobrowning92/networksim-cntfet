3ab34a62df8fea2207c1949069351c95277d52b9
Leo Browning <leobrowning92@gmail.com>
2018-07-19 09:46:11 +1200

    added unique file/job names in batch submission

--- slurm/batchmeasure.sh
+++ slurm/batchmeasure.sh
@@ -9,8 +9,8 @@ do
         n=$(echo $density*3600 | bc)
         for x in {1..10}
         do
-            echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$x > mnet$density's'$x.sh
-            subpy -P 1 -t 2-0 mnet$density's'$x.sh
+            echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$x > mnet$density'om'$omap's'$x.sh
+            subpy -P 1 -t 2-0 mnet$density'om'$omap's'$x.sh
         done
     done
 done
