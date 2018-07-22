d61414a5befb3792eaf6bbd5017674434cca34b1
Leo Browning <leobrowning92@gmail.com>
2018-07-23 09:19:37 +1200

    changed measurment to finer density steps and random seeds

--- slurm/batchmeasure.sh
+++ slurm/batchmeasure.sh
@@ -2,16 +2,19 @@
 # this dictates the number of measurements to be made, and for what densities
 mkdir data
 cd data
-for omap in {0..2}
+for omap in {0..1}
 do
-    for density in {9..14}
+    for step in {0..50}
     do
+        density=$(echo 9+0.04*$step | bc)
+        echo $density
         n=$(echo $density*3600 | bc)
-        for x in {1..10}
-        do
-            echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$x > mnet$density'om'$omap's'$x.sh
-            subpy -P 1 -t 2-0 mnet$density'om'$omap's'$x.sh
-        done
+        echo $n
+        echo $omap
+
+        echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1' > mnet$density'om'$omap.sh
+        subpy -P 1 -t 2-0 mnet$density'om'$omap.sh
+
     done
 done
 cd ..
