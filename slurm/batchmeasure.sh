87901df582a8146d083fb7e838966de7f3f54381
Leo Browning <leobrowning92@gmail.com>
2018-07-23 09:22:46 +1200

    removed uneccessary prints form batchmeasure

--- slurm/batchmeasure.sh
+++ slurm/batchmeasure.sh
@@ -7,11 +7,7 @@ do
     for step in {0..50}
     do
         density=$(echo 9+0.04*$step | bc)
-        echo $density
         n=$(echo $density*3600 | bc)
-        echo $n
-        echo $omap
-
         echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1' > mnet$density'om'$omap.sh
         subpy -P 1 -t 2-0 mnet$density'om'$omap.sh
 
