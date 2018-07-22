3dfead3fb60b0bab5afc7366c1220c2bf8a41b65
Leo Browning <leobrowning92@gmail.com>
2018-07-23 09:31:00 +1200

    added integer rounding to n in batch submission

--- slurm/batchmeasure.sh
+++ slurm/batchmeasure.sh
@@ -8,6 +8,7 @@ do
     do
         density=$(echo 9+0.04*$step | bc)
         n=$(echo $density*3600 | bc)
+        n=${n%.*}
         echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1' > mnet$density'om'$omap.sh
         subpy -P 1 -t 2-0 mnet$density'om'$omap.sh
 
