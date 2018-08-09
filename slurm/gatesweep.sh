d680b98b445324275ebcf1d517338650213dc2b2
Leo Browning <leobrowning92@gmail.com>
2018-08-09 16:09:30 +1200

    edited gatesweep to avoid duplicate filenames

--- slurm/gatesweep.sh
+++ slurm/gatesweep.sh
@@ -14,29 +14,29 @@ do
     seed=1297489092
     n=$(echo $density*3600 | bc)
     n=${n%.*}
-    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$seed' --vgnum=11' > mnet$density's'$seed.sh
-    subpy -P 1 -t 2-0 mnet$density's'$seed.sh
+    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$seed' --vgnum=11' > mns$seed.sh
+    subpy -P 1 -t 2-0 mns$seed.sh
 
     density=9.5
     seed=1296295872
     n=$(echo $density*3600 | bc)
     n=${n%.*}
-    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$seed' --vgnum=11' > mnet$density's'$seed.sh
-    subpy -P 1 -t 2-0 mnet$density's'$seed.sh
+    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$seed' --vgnum=11' > mns$seed.sh
+    subpy -P 1 -t 2-0 mns$seed.sh
 
     density=11.5
     seed=11197951452
     n=$(echo $density*3600 | bc)
     n=${n%.*}
-    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$seed' --vgnum=11' > mnet$density's'$seed.sh
-    subpy -P 1 -t 2-0 mnet$density's'$seed.sh
+    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$seed' --vgnum=11' > mns$seed.sh
+    subpy -P 1 -t 2-0 mns$seed.sh
 
     density=11.5
     seed=1138375167
     n=$(echo $density*3600 | bc)
     n=${n%.*}
-    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$seed' --vgnum=11' > mnet$density's'$seed.sh
-    subpy -P 1 -t 2-0 mnet$density's'$seed.sh
+    echo 'python3 ~/gitrepos/networksim-cntfet/measure_perc.py singlecore -s -v  -n '$n' --scaling 60 --onoffmap '$omap' --element 1 --seed '$seed' --vgnum=11' > mns$seed.sh
+    subpy -P 1 -t 2-0 mns$seed.sh
 
 
 done
